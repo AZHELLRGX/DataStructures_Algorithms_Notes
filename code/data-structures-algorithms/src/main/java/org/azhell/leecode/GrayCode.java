f0ded960a9ed6bc814b67f92dd45559a9156f9cb
AZHELLRGX <776379772@qq.com>
2022-01-08 13:40:15 +0800

    89. 格雷编码【位运算】

--- code/data-structures-algorithms/src/main/java/org/azhell/leecode/GrayCode.java
+++ code/data-structures-algorithms/src/main/java/org/azhell/leecode/GrayCode.java
@@ -15,6 +15,26 @@ public class GrayCode {
     public static void main(String[] args) {
         GrayCode g = new GrayCode();
         Utils.print(g.grayCode(3));
+        Utils.print(g.grayCode1(3));
+    }
+
+    public List<Integer> grayCode1(int n) {
+        /*
+         关键是搞清楚格雷编码的生成过程, G(i) = i ^ (i/2);
+         如 n = 3:
+         G(0) = 000,
+         G(1) = 1 ^ 0 = 001 ^ 000 = 001
+         G(2) = 2 ^ 1 = 010 ^ 001 = 011
+         G(3) = 3 ^ 1 = 011 ^ 001 = 010
+         G(4) = 4 ^ 2 = 100 ^ 010 = 110
+         G(5) = 5 ^ 2 = 101 ^ 010 = 111
+         G(6) = 6 ^ 3 = 110 ^ 011 = 101
+         G(7) = 7 ^ 3 = 111 ^ 011 = 100
+         */
+        List<Integer> ret = new ArrayList<>();
+        for (int i = 0; i < 1 << n; ++i)
+            ret.add(i ^ i >> 1);
+        return ret;
     }
 
     public List<Integer> grayCode(int n) {
