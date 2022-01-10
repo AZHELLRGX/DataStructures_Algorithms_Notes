68a8c4bfd05f97dd54aa48ad3b4219c83e1077a3
AZHELLRGX <776379772@qq.com>
2022-01-10 15:06:59 +0800

    43.字符串相乘

--- code/data-structures-algorithms/src/main/java/org/azhell/leecode/MultiplyStrings.java
+++ code/data-structures-algorithms/src/main/java/org/azhell/leecode/MultiplyStrings.java
@@ -3,14 +3,14 @@ package org.azhell.leecode;
 import org.azhell.tool.Utils;
 
 /**
- * 字符串相乘
+ * 43.字符串相乘
  * 步骤模拟
- * 需要考虑整型数据溢出的情况
- * 不允许使用大数类型处理，比如long类型
- * todo 等后面做完字符串加法再回来解决这个题，代码可以复用
+ * num1的第i位(高位从0开始)和num2的第j位相乘的结果在乘积中的位置是[i+j, i+j+1]
+ * 所以使用数组来存储各位相乘以后的结果
  */
 public class MultiplyStrings {
     public static void main(String[] args) {
+        Utils.print(new MultiplyStrings().multiply("45", "123"));
         Utils.print(new MultiplyStrings().multiply("123", "456"));
         Utils.print(new MultiplyStrings().multiply("2", "3"));
         Utils.print(new MultiplyStrings().multiply("2", "34"));
@@ -19,14 +19,27 @@ public class MultiplyStrings {
     }
 
     public String multiply(String num1, String num2) {
-        long result = 0L;
+        if ("0".equals(num1) || "0".equals(num2)) {
+            return "0";
+        }
+        int[] ansArray = new int[num1.length() + num2.length()];
         for (int i = num1.length() - 1; i >= 0; i--) {
-            int temp1 = num1.charAt(i) - '0';
+            int n1 = num1.charAt(i) - '0';
             for (int j = num2.length() - 1; j >= 0; j--) {
-                int temp2 = num2.charAt(j) - '0';
-                result += temp1 * temp2 * Math.pow(10, (num1.length() - 1 - i)) * Math.pow(10, (num2.length() - 1 - j));
+                int n2 = num2.charAt(j) - '0';
+                // 与之前的数据相加，判断是否存在进位
+                int res = n1 * n2 + ansArray[i + j + 1];
+                ansArray[i + j] += res / 10;
+                ansArray[i + j + 1] = res % 10;
             }
         }
-        return String.valueOf(result);
+        StringBuilder sb = new StringBuilder();
+        if (ansArray[0] != 0) {
+            sb.append(ansArray[0]);
+        }
+        for (int i = 1; i < ansArray.length; i++) {
+            sb.append(ansArray[i]);
+        }
+        return sb.toString();
     }
 }
