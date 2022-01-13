a4e456cc7292dbe69fb0bddc3f41803a6022d2d7
AZHELLRGX <776379772@qq.com>
2022-01-13 11:35:02 +0800

    反转链表【迭代解法】

--- code/data-structures-algorithms/src/main/java/org/azhell/leecode/sword/Offer24.java
+++ code/data-structures-algorithms/src/main/java/org/azhell/leecode/sword/Offer24.java
@@ -14,13 +14,38 @@ import java.util.Deque;
 public class Offer24 {
     public static void main(String[] args) {
         Offer24 o = new Offer24();
-        final ListNode head = Utils.initLinkedList(new int[]{1, 2, 3, 4, 5});
+        ListNode head = Utils.initLinkedList(new int[]{1, 2, 3, 4, 5});
         Utils.print(o.reverseList(head));
+        head = Utils.initLinkedList(new int[]{1, 2, 3, 4, 5});
+        Utils.print(o.reverseList1(head));
+        head = Utils.initLinkedList(new int[]{1});
+        Utils.print(o.reverseList(head));
+        head = Utils.initLinkedList(new int[]{1});
+        Utils.print(o.reverseList1(head));
     }
 
+    // 使用迭代解题【效率直接100%】
     public ListNode reverseList(ListNode head) {
-        if (head == null){
-            return null;
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode curNode = head.next;
+        ListNode preNode = head;
+        while (curNode.next != null) {
+            ListNode tempNode = curNode;
+            curNode = curNode.next;
+            tempNode.next = preNode;
+            preNode = tempNode;
+        }
+        curNode.next = preNode;
+        head.next = null;
+        return curNode;
+    }
+
+    // 栈的效率较低
+    public ListNode reverseList1(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
         }
         Deque<ListNode> stack = new ArrayDeque<>();
         while (head != null) {
