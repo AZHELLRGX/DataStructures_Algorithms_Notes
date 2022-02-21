ecf2709c8bc9a0a2a9288e3dd1a0275f726e43ae
AZHELLRGX <776379772@qq.com>
2022-02-21 18:09:47 +0800

    746. 使用最小花费爬楼梯【代码简化】

--- code/data-structures-algorithms/src/main/java/org/azhell/leecode/MinCostClimbingStairs.java
+++ code/data-structures-algorithms/src/main/java/org/azhell/leecode/MinCostClimbingStairs.java
@@ -17,12 +17,12 @@ public class MinCostClimbingStairs {
 
     public int minCostClimbingStairs(int[] cost) {
         int n = cost.length;
-        int[] dp = new int[n + 1];
+        int[] dp = new int[n];
         dp[0] = cost[0];
         dp[1] = cost[1];
-        for (int i = 2; i <= n; i++) {
-            dp[i] = (i < n ? cost[i] : 0) + Math.min(dp[i - 1], dp[i - 2]);
+        for (int i = 2; i < n; i++) {
+            dp[i] = cost[i] + Math.min(dp[i - 1], dp[i - 2]);
         }
-        return dp[n];
+        return Math.min(dp[n - 1], dp[n - 2]);
     }
 }
