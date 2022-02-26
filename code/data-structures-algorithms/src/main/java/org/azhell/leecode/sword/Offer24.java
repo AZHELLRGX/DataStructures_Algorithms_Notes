6962bf2911d5cda65596ea59e3764996827c814b
AZHELLRGX <776379772@qq.com>
2022-02-26 10:43:04 +0800

    反转链表【迭代算法简化、增加尾递归解法】

--- code/data-structures-algorithms/src/main/java/org/azhell/leecode/sword/Offer24.java
+++ code/data-structures-algorithms/src/main/java/org/azhell/leecode/sword/Offer24.java
@@ -10,6 +10,7 @@ import java.util.Deque;
  * 剑指 Offer 24. 反转链表
  * 206. 反转链表
  * 还是借助栈来解决
+ * 除了栈之外，还可以使用迭代法和尾递归
  */
 public class Offer24 {
     public static void main(String[] args) {
@@ -18,6 +19,8 @@ public class Offer24 {
         Utils.print(o.reverseList(head));
         head = Utils.initLinkedList(new int[]{1, 2, 3, 4, 5});
         Utils.print(o.reverseList1(head));
+        head = Utils.initLinkedList(new int[]{1, 2, 3, 4, 5});
+        Utils.print(o.reverseList2(head));
         head = Utils.initLinkedList(new int[]{1});
         Utils.print(o.reverseList(head));
         head = Utils.initLinkedList(new int[]{1});
@@ -26,21 +29,30 @@ public class Offer24 {
 
     // 使用迭代解题【效率直接100%】
     public ListNode reverseList(ListNode head) {
-        if (head == null || head.next == null) {
-            return head;
-        }
-        ListNode curNode = head.next;
-        ListNode preNode = head;
-        while (curNode.next != null) {
+        ListNode curNode = head;
+        ListNode preNode = null;
+        while (curNode != null) {
             ListNode tempNode = curNode;
             curNode = curNode.next;
             tempNode.next = preNode;
             preNode = tempNode;
         }
-        curNode.next = preNode;
-        head.next = null;
-        return curNode;
+        return preNode;
     }
+
+    // 尾递归解法；这个递归解法思路比较巧妙
+    public ListNode reverseList2(ListNode head) {
+        return reverse(head, null);
+    }
+
+    private ListNode reverse(ListNode cur, ListNode pre) {
+        if (cur == null) return pre;
+        ListNode tempNode = cur.next;
+        cur.next = pre;
+        // 直接往后递推
+        return reverse(tempNode, cur);
+    }
+
 
     // 栈的效率较低
     public ListNode reverseList1(ListNode head) {
